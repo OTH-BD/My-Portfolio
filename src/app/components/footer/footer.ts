ca1f5d508eda8953e4bc1e37304025ee105ad427
OTH-BD <othmane.boudali01@gmail.com>
2025-07-10 18:47:03 +0100

    update v2

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
+import { About } from '../../pages/about/about';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule,],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
 export class Footer {
-  
+  phoneNumber: string = '+212695282305'; // Remplacez par votre numéro de téléphone
+  email: string = 'othmane.boudali01@gmail.com';
+  github: string = 'https://github.com/OTH-BD';
+  linkedin: string = 'https://www.linkedin.com/in/othmane-boudali-80344a21a/';
+  instagram: string = 'https://www.instagram.com/othmaneboudali/';
 
 }
